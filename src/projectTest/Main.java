f71e3ee0716ad8d3be87fb757c882a89e965d578
skoggar <skoggar.art@gmail.com>
2014-09-27 13:51:27 +0200

    x

--- src/projectTest/Main.java
+++ src/projectTest/Main.java
@@ -3,8 +3,8 @@ package projectTest;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello java");
+		
+		System.out.println("hello mpr");
 	}
 
 }
